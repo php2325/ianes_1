d8a8c9df1ca17aae81749be5a8f1f6823c3f16c7
php2325 <petdb24php@gmail.com>
2026-02-02 10:48:39 -0300

    Primeiro projeto Spring boot JAVA

---
+++ src/test/java/com/pbe/ianes/IanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbe.ianes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
